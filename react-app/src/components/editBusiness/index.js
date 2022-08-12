61ac53d69d2ed8a4f3a8f77467605dc3c4166178
Christian Alcantara <chriscayana@gmail.com>
2022-08-12 14:57:18 -0700

    changed validator

--- react-app/src/components/editBusiness/index.js
+++ react-app/src/components/editBusiness/index.js
@@ -61,12 +61,12 @@ function EditBusiness ({business, setShowModal}) {
         }else if(!website.includes('www')) {
           errorsObj.website = "Please enter a valid website starting with 'www'"
           error = true;
-        }else if (!imageURL.includes(".jpg") && !imageURL.includes(".png") && !imageURL.includes(".JPG") && !imageURL.includes(".PNG") && !imageURL.includes("image")) {
-          errorsObj.imageURL = "imageURL must be jpg/png or contain image"
+        }else if (!imageURL.includes(".jpg") && !imageURL.includes(".png") && !imageURL.includes(".JPG") && !imageURL.includes(".PNG") && !imageURL.includes("image") && !imageURL.includes(".JPEG") && !imageURL.includes(".jpeg")) {
+          errorsObj.imageURL = "Logo image must be jpg/png/jpeg or contain image"
           error = true
         }
         else if (imageURL.length < 4) {
-          errorsObj.imageURL = "imageURL must be at least 4 characters."
+          errorsObj.imageURL = "Logo image must be at least 4 characters."
           error = true
         }
         setErrors(errorsObj);
